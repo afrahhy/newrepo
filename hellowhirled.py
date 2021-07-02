601c9d1da92dfebca96c8a0e77f65229e3941544
afrahhy <afrahhyder@gmail.com>
2021-07-02 14:04:29 +0100

    simplified and removed function

--- hellowhirled.py
+++ hellowhirled.py
@@ -1,22 +1,22 @@
-# input('Anyone there?')
-print('hello whirled')
+input('Anyone there?')
+print('hello werld')
 answer = input('did I spell that right?' )
 answer = answer.lower()
 
-def understandanswer(answer):
-    if answer == 'yes' or 'yeah' or 'yh' or 'yea' or 'no':
-        return True
-    else:
-        return False
+# def understandanswer(answer):
+#     if answer == 'yes' or 'yeah' or 'yh' or 'yea' or 'no':
+#         return True
+#     else:
+#         return False
 
-print(understandanswer(answer))
+# print(understandanswer(answer))
 
-# if not bool(answer):
-#     answer = input("I didn't understand, could you repeat that?")
+if answer != 'yes' or 'no':
+    answer = input("I didn't understand, could you repeat that?")
 
-# if answer == 'yes':
-#     print('Okay, thanks')
-# elif answer == 'no':
-#         print('sorry, i meant to say "hello world"')
-# else:
-#     print("oops")
+if answer == 'yes':
+    print('Okay, thanks')
+elif answer == 'no':
+        print('sorry, i meant to say "hello world"')
+else:
+    print("oops")
