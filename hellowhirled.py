e8a477b1db4c9e7aaa142ce59e4fefe699c40bcb
afrahhy <afrahhyder@gmail.com>
2021-07-02 14:12:41 +0100

    deleted wonky function
    function always return true, or when true/false was assigned to separate variable, always returned none

--- hellowhirled.py
+++ hellowhirled.py
@@ -1,22 +1,14 @@
-# input('Anyone there?')
+input('Anyone there?')
 print('hello whirled')
 answer = input('did I spell that right?' )
 answer = answer.lower()
 
-def understandanswer(answer):
-    if answer == 'yes' or 'yeah' or 'yh' or 'yea' or 'no':
-        return True
-    else:
-        return False
+if answer != 'yes' or 'no':
+    answer = input("I didn't understand, could you repeat that?")
 
-print(understandanswer(answer))
-
-# if not bool(answer):
-#     answer = input("I didn't understand, could you repeat that?")
-
-# if answer == 'yes':
-#     print('Okay, thanks')
-# elif answer == 'no':
-#         print('sorry, i meant to say "hello world"')
-# else:
-#     print("oops")
+if answer == 'yes':
+    print('Okay, thanks')
+elif answer == 'no':
+        print('sorry, i meant to say "hello world"')
+else:
+    print("oops")
