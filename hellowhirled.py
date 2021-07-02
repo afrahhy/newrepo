60e6939496618f0ade835f459c8cb1cede5803c1
afrahhy <afrahhyder@gmail.com>
2021-07-02 13:44:55 +0100

    non y/n answers
    allowed for a reprompt for a non yes or no answer

--- hellowhirled.py
+++ hellowhirled.py
@@ -1,8 +1,15 @@
 input('Anyone there?')
-print('hello whirled")
-answer = input('did I spell that right?': )
+print('hello whirled')
+answer = input('did I spell that right?' )
 answer = answer.lower()
-if answer == 'yes'
+
+if not bool(answer=='yes') or not bool(answer=='no'):
+    answer = input("I didn't understand, could you repeat that?")
+
+if answer == 'yes':
     print('Okay, thanks')
-if answer == 'no'
-    print('sorry, i meant to say "hello world"')
+if answer == 'no':
+        print('sorry, i meant to say "hello world"')
+# else:
+#     print("I didn't understand, can you repeat that?")
+#     answer = input
