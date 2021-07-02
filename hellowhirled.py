f959a1986f8c91093ca40d3a2de1d59a2b858867
afrahhy <afrahhyder@gmail.com>
2021-07-02 13:03:08 +0100

    python file, more interaction
    changed to a python file, added input and print responses for 'yes' and 'no'

---
+++ hellowhirled.py
@@ -0,0 +1,8 @@
+input('Anyone there?')
+print('hello whirled")
+answer = input('did I spell that right?': )
+answer = answer.lower()
+if answer == 'yes'
+    print('Okay, thanks')
+if answer == 'no'
+    print('sorry, i meant to say "hello world"')
